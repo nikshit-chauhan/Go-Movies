c0099fb3bf1cd27e814fe3ff0669cf61222f4dff
Nikshit Chauhan <nikshit2012@gmail.com>
2023-03-18 19:08:43 +0530

    added movie repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
+import 'package:dio/dio.dart';
 
 void main() {
   runApp(const ProviderScope(child: App()));
 }
+
+final dioProvider = Provider<Dio>((ref) {
+  return Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3/',
+  ));
+});
